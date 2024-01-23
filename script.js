05807160234c666c44dac0ac40120b2a04f6531d
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-23 13:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    if (num < 1 || num > 100000) {
+        return "Number out of range. Please provide a number between 1 and 100000.";
+    }
 
-  //your code here
-	 let result = "";
+    const romanNumerals = [
+        { value: 100000, symbol: "C̅" },
+        { value: 90000, symbol: "X̅C̅" },
+        { value: 50000, symbol: "L̅" },
+        { value: 40000, symbol: "X̅L̅" },
+        { value: 10000, symbol: "X̅" },
+        { value: 9000, symbol: "I̅X̅" },
+        { value: 5000, symbol: "V̅" },
+        { value: 4000, symbol: "I̅V̅" },
+        { value: 1000, symbol: "M" },
+        { value: 900, symbol: "CM" },
+        { value: 500, symbol: "D" },
+        { value: 400, symbol: "CD" },
+        { value: 100, symbol: "C" },
+        { value: 90, symbol: "XC" },
+        { value: 50, symbol: "L" },
+        { value: 40, symbol: "XL" },
+        { value: 10, symbol: "X" },
+        { value: 9, symbol: "IX" },
+        { value: 5, symbol: "V" },
+        { value: 4, symbol: "IV" },
+        { value: 1, symbol: "I" }
+    ];
+
+    let result = "";
 
     for (const numeral of romanNumerals) {
         while (num >= numeral.value) {
@@ -22,11 +39,10 @@ function convertToRoman(num) {
     return result;
 }
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(convertToRoman(36));
-
+// Example usage:
+const inputNumber = 798;
+const romanNumeral = convertToRoman(inputNumber);
+console.log(romanNumeral); // Output: DCCXCVIII
 
 
 
